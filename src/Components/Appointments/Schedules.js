90908df6ec3e3308997b2b4a51f5729dad68d42e
Bhargav R G <bhargavr445@gmail.com>
2026-02-20 16:36:40 -0500

    upd

--- src/Components/Appointments/Schedules.js
+++ src/Components/Appointments/Schedules.js
@@ -1,5 +1,6 @@
-import { memo } from 'react'
-function Schedules({ updateNameFn }) 
+import { memo } from "react";
+
+function Schedules({ updateNameFn }) {
   return (
     <div>
       <button onClick={updateNameFn}>Update Name</button>
